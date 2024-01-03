b90116f38a73d953964bfc627aed25658c14f2de
JackyWuCoder <JackyWuCoder@gmail.com>
2024-01-02 18:33:12 -0800

    Unit 2 Lesson 4, Created Enemy

--- Assets/Scripts/Enemies/MeleeEnemy.cs
+++ Assets/Scripts/Enemies/MeleeEnemy.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class MeleeEnemy : Enemy
 {
-    public float attackRange;
+    [SerializeField] private float attackRange;
+    [SerializeField] private float attackTime = 0f;
 
+    private float timer = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+        health = new Health(1, 1, 0);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (target = null)
+        {
+            return;
+        }
+        if(Vector2.Distance(transform.position, target.position) < attackRange)
+        {
+            Attack(attackTime);
+        }
+    }
 }
